3f903c0bf04655e4ac253310c42450360cdd2120
VADUNG <vad24042002@gmail.com>
2024-04-06 16:02:42 +0700

    update

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
+const express = require('express');
+require('dotenv').config()
 
 import cors from "cors";
 import initRoutes from "./src/routes/index.js";
